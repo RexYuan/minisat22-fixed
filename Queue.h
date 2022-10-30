a3a1d565faa2e9c4ad8c44faf906ee1934e86a38
RexYuan <r1218r1218@gmail.com>
2022-10-30 23:01:18 +0800

    Imported from MiniSat_v1.12b_src.zip on http://minisat.se/MiniSat.html

--- Queue.h
+++ Queue.h
@@ -1,5 +1,5 @@
-/****************************************************************************************[XAlloc.h]
-Copyright (c) 2009-2010, Niklas Sorensson
+/**************************************************************************************************
+MiniSat -- Copyright (c) 2003-2005, Niklas Een, Niklas Sorensson
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -17,29 +17,29 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **************************************************************************************************/
 
+#ifndef Queue_h
+#define Queue_h
 
-#ifndef Minisat_XAlloc_h
-#define Minisat_XAlloc_h
-
-#include <errno.h>
-#include <stdlib.h>
-
-namespace Minisat {
 
 //=================================================================================================
-// Simple layer on top of malloc/realloc to catch out-of-memory situtaions and provide some typing:
 
-class OutOfMemoryException{};
-static inline void* xrealloc(void *ptr, size_t size)
-{
-    void* mem = realloc(ptr, size);
-    if (mem == NULL && errno == ENOMEM){
-        throw OutOfMemoryException();
-    }else
-        return mem;
-}
+
+template <class T>
+class Queue {
+    vec<T>  elems;
+    int     first;
+
+public:
+    Queue(void) : first(0) { }
+
+    void insert(T x)   { elems.push(x); }
+    T    dequeue(void) { return elems[first++]; }
+    void clear(void)   { elems.clear(); first = 0; }
+    int  size(void)    { return elems.size() - first; }
+
+    bool has(T x) { for (int i = first; i < elems.size(); i++) if (elems[i] == x) return true; return false; }
+};
+
 
 //=================================================================================================
-}
-
 #endif
